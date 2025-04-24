69e24ccf19c2d743388743cc4123134292a0b549
rxliuli <rxliuli@gmail.com>
2025-04-24 17:39:16 +0800

    chore: update CFBundleVersion on safari

--- packages/plugin/lib/build/build-safari.ts
+++ packages/plugin/lib/build/build-safari.ts
@@ -1,4 +1,4 @@
-import { $ } from 'zx'
+import { $, globby } from 'zx'
 import path from 'node:path'
 import fs from 'node:fs/promises'
 import dotenv from 'dotenv'
@@ -12,25 +12,47 @@ const DevelopmentTeam = process.env.DEVELOPMENT_TEAM
 
 await $`pnpm wxt build -b safari`
 await $`xcrun safari-web-extension-converter --bundle-identifier com.rxliuli.corsUnblock --force --project-location .output .output/safari-mv3`
-const projectConfigPath = path.resolve(
-  rootPath,
-  `.output/${ProjectName}/${ProjectName}.xcodeproj/project.pbxproj`,
-)
-const packageJson = await import(path.resolve(rootPath, 'package.json'))
-const content = await fs.readFile(projectConfigPath, 'utf-8')
-await fs.writeFile(
-  projectConfigPath,
-  content
-    .replaceAll(
-      'MARKETING_VERSION = 1.0;',
-      `MARKETING_VERSION = ${packageJson.version};`,
+async function updateProjectConfig() {
+  const projectConfigPath = path.resolve(
+    rootPath,
+    `.output/${ProjectName}/${ProjectName}.xcodeproj/project.pbxproj`,
+  )
+  const packageJson = await import(path.resolve(rootPath, 'package.json'))
+  const content = await fs.readFile(projectConfigPath, 'utf-8')
+  await fs.writeFile(
+    projectConfigPath,
+    content
+      .replaceAll(
+        'MARKETING_VERSION = 1.0;',
+        `MARKETING_VERSION = ${packageJson.version};`,
+      )
+      .replaceAll(
+        `INFOPLIST_KEY_CFBundleDisplayName = "${ProjectName}";`,
+        `INFOPLIST_KEY_CFBundleDisplayName = "${ProjectName}";\n				INFOPLIST_KEY_LSApplicationCategoryType = "${AppCategory}";`,
+      )
+      .replaceAll(
+        `COPY_PHASE_STRIP = NO;`,
+        `COPY_PHASE_STRIP = NO;\n				DEVELOPMENT_TEAM = ${DevelopmentTeam};`,
+      ),
+  )
+}
+
+async function updateInfoPlist() {
+  const projectPath = path.resolve(rootPath, '.output', ProjectName)
+  const files = await globby('**/*.plist', {
+    cwd: projectPath,
+  })
+  for (const file of files) {
+    const content = await fs.readFile(path.resolve(projectPath, file), 'utf-8')
+    await fs.writeFile(
+      path.resolve(projectPath, file),
+      content.replaceAll(
+        '</dict>\n</plist>',
+        '	<key>CFBundleVersion</key>\n	<string>$(MARKETING_VERSION)</string>\n</dict>\n</plist>',
+      ),
     )
-    .replaceAll(
-      `INFOPLIST_KEY_CFBundleDisplayName = "${ProjectName}";`,
-      `INFOPLIST_KEY_CFBundleDisplayName = "${ProjectName}";\n				INFOPLIST_KEY_LSApplicationCategoryType = "${AppCategory}";`,
-    )
-    .replaceAll(
-      `COPY_PHASE_STRIP = NO;`,
-      `COPY_PHASE_STRIP = NO;\n				DEVELOPMENT_TEAM = ${DevelopmentTeam};`,
-    ),
-)
+  }
+}
+
+await updateProjectConfig()
+await updateInfoPlist()
